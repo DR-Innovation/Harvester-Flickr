5e209c7b7e543c87522256e26e72d7b7986dd847
Kræn Hansen <post@creen.dk>
2013-02-01 23:37:23 +0100

    Just fooling around ..

--- src/CHAOS/Harvester/Flickr/Processors/PhotoObjectProcessor.php
+++ src/CHAOS/Harvester/Flickr/Processors/PhotoObjectProcessor.php
@@ -5,6 +5,6 @@ namespace CHAOS\Harvester\Flickr\Processors;
 class PhotoObjectProcessor extends \CHAOS\Harvester\Processors\ObjectProcessor {
 	
 	public function process($externalObject, $shadow = null) {
-		echo "process called ...";
+		//var_dump($externalObject);
 	}
 }
